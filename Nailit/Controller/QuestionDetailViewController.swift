98074c03ccf6edea7c11a685dd14c58d99f2d2e3
PAW123 <gedeprama123@gmail.com>
2021-04-13 14:38:31 +0700

    Initial Commit

---
+++ Nailit/Controller/QuestionDetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  QuestionDetailViewController.swift
+//  Nailit
+//
+//  Created by Gede Wicaksana on 12/04/21.
+//
+
+import UIKit
+import CoreData
+
+class QuestionDetailViewController: UIViewController {
+    
+    @IBOutlet weak var questionTitle: UILabel!
+    @IBOutlet weak var detailQuestion1: UITextView!
+    @IBOutlet weak var detailQuestion2: UITextView!
+    
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var  question = ""
+    var questionDetail1 = ""
+    var questionDetail2 = ""
+    var arrayDetailQuestion = [ListOfQuestion]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionTitle.text = question
+        detailQuestion1.text = questionDetail1
+        detailQuestion2.text = questionDetail2
+    }
+    
+    // tombol bookmark disentuh atas dalam
+    @IBAction func bookmarkButtonOnClick(_ sender: UIBarButtonItem) {
+        InsertIntoBookmark()
+    }
+    
+
+    
+    // MARK: - Set data
+    func InsertIntoBookmark(){
+        let request : NSFetchRequest<ListOfQuestion> = ListOfQuestion.fetchRequest()
+        request.predicate = NSPredicate(format: "question = %@",question)
+        do {
+            arrayDetailQuestion = try context.fetch(request)
+            arrayDetailQuestion[0].setValue(true, forKey: "bookmark")
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving content: \(error)")
+        }
+    }
+    
+    
+}
